b00aa5f612f5c378e5d8b0ffb93cfced09d95f18
jordanrowe <jordan.rowe@digital.hmrc.gov.uk>
2020-04-11 14:12:29 +0100

    NIC Calculation for Monthly and One Pay Period

--- test/base/SpecBaseWithApplication.scala
+++ test/base/SpecBaseWithApplication.scala
@@ -20,7 +20,8 @@ import play.api.mvc.AnyContentAsEmpty
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 
-trait SpecBaseWithApplication extends PlaySpec with GuiceOneAppPerSuite with TryValues with ScalaFutures with IntegrationPatience {
+trait SpecBaseWithApplication
+    extends PlaySpec with GuiceOneAppPerSuite with TryValues with ScalaFutures with IntegrationPatience {
 
   val userAnswersId = "id"
 
--- test/services/NicCalculatorServiceSpec.scala
+++ test/services/NicCalculatorServiceSpec.scala
@@ -21,14 +21,46 @@ class NicCalculatorServiceSpec extends SpecBase with ScalaCheckPropertyChecks {
 
   private lazy val scenarios = Table(
     ("paymentFrequency", "FurloughPayment", "taxYear", "expected"),
-    (Monthly, FurloughPayment(700.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))), TaxYearEnding2020, 0.00),
-    (Monthly, FurloughPayment(1000.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))), TaxYearEnding2020, 38.77),
-    (Monthly, FurloughPayment(5000.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))), TaxYearEnding2020, 245.77),
-    (Monthly, FurloughPayment(700.00, PayPeriod(LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 31))), TaxYearEnding2021, 0.00),
-    (Monthly, FurloughPayment(1000.00, PayPeriod(LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 31))), TaxYearEnding2021, 36.98),
-    (Monthly, FurloughPayment(5000.00, PayPeriod(LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 31))), TaxYearEnding2021, 243.98),
-    (FourWeekly, FurloughPayment(600.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))), TaxYearEnding2020, 0.00),
-    (FourWeekly, FurloughPayment(700.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))), TaxYearEnding2020, 4.96)
+    (
+      Monthly,
+      FurloughPayment(700.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))),
+      TaxYearEnding2020,
+      0.00),
+    (
+      Monthly,
+      FurloughPayment(1000.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))),
+      TaxYearEnding2020,
+      38.77),
+    (
+      Monthly,
+      FurloughPayment(5000.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))),
+      TaxYearEnding2020,
+      245.77),
+    (
+      Monthly,
+      FurloughPayment(700.00, PayPeriod(LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 31))),
+      TaxYearEnding2021,
+      0.00),
+    (
+      Monthly,
+      FurloughPayment(1000.00, PayPeriod(LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 31))),
+      TaxYearEnding2021,
+      36.98),
+    (
+      Monthly,
+      FurloughPayment(5000.00, PayPeriod(LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 31))),
+      TaxYearEnding2021,
+      243.98),
+    (
+      FourWeekly,
+      FurloughPayment(600.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))),
+      TaxYearEnding2020,
+      0.00),
+    (
+      FourWeekly,
+      FurloughPayment(700.00, PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))),
+      TaxYearEnding2020,
+      4.96)
   )
 
 }
