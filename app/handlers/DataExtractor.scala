05ec7626a8196fd8f8cba4ed70e370c77e72cbcd
Pasquale Gatto <pasquale.gatto@hmrc.digital.gov.uk>
2020-04-16 16:18:59 +0100

    variable_pay_V2: extract Furlough dates

--- app/handlers/DataExtractor.scala
+++ app/handlers/DataExtractor.scala
@@ -7,7 +7,9 @@ package handlers
 
 import java.time.LocalDate
 
-import models.{FurloughQuestion, NicCategory, PayPeriod, PayQuestion, PaymentFrequency, PensionStatus, UserAnswers}
+import models.FurloughDates.StartedInClaim
+import models.FurloughQuestion.{No, Yes}
+import models.{ClaimPeriodModel, FurloughDates, FurloughPeriod, FurloughQuestion, NicCategory, PayPeriod, PayQuestion, PaymentFrequency, PensionStatus, UserAnswers}
 import pages._
 
 case class MandatoryData(
@@ -33,4 +35,29 @@ trait DataExtractor {
       payDate = userAnswers.getList(PayDatePage)
     } yield MandatoryData(PayPeriod(claimStart, claimEnd), frequency, nic, pension, payQuestion, furlough, payDate)
 
+  def extractFurloughPeriod(userAnswers: UserAnswers): Option[FurloughPeriod] =
+    for {
+      data <- extract(userAnswers)
+      furloughDates = userAnswers.get(FurloughDatesPage)
+      furloughStart = userAnswers.get(FurloughStartDatePage)
+    } yield {
+      data.furloughQuestion match {
+        case Yes => FurloughPeriod(data.claimPeriod.start, data.claimPeriod.end)
+        case No =>
+          processFurloughDates(data, furloughDates, furloughStart)
+            .getOrElse(FurloughPeriod(data.claimPeriod.start, data.claimPeriod.end)) //TODO fix this
+      }
+    }
+
+  private def processFurloughDates(
+    data: MandatoryData,
+    furloughDates: Option[FurloughDates],
+    furloughStart: Option[LocalDate]): Option[FurloughPeriod] =
+    for {
+      start <- furloughStart
+    } yield {
+      furloughDates match {
+        case Some(StartedInClaim) => FurloughPeriod(start, data.claimPeriod.end)
+      }
+    }
 }
--- test/handlers/DataExtractorSpec.scala
+++ test/handlers/DataExtractorSpec.scala
@@ -5,18 +5,36 @@
 
 package handlers
 
+import java.time.LocalDate
+
 import base.SpecBase
-import models.UserAnswers
+import models.{FurloughPeriod, PayPeriod, UserAnswers}
 import play.api.libs.json.Json
 import utils.CoreTestData
 
 class DataExtractorSpec extends SpecBase with CoreTestData {
 
   "Extract mandatory data in order to do the calculation" in new DataExtractor {
-    val userAnswers = Json.parse(userAnswersJson).as[UserAnswers]
+    val userAnswers = Json.parse(userAnswersJson()).as[UserAnswers]
 
     extract(userAnswers) must matchPattern {
       case Some(MandatoryData(_, _, _, _, _, _, _)) =>
     }
+  }
+
+  "Extract furlough period matching claim period if furlough question entered is yes" in new DataExtractor {
+    val userAnswers = Json.parse(userAnswersJson("yes")).as[UserAnswers]
+    val claimPeriod = PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 4, 30))
+    val expected = FurloughPeriod(claimPeriod.start, claimPeriod.end)
+
+    extractFurloughPeriod(userAnswers) mustBe Some(expected)
+  }
+
+  "Extract furlough period with end date matching the claim end date with user submitted start date" in new DataExtractor {
+    val userAnswers = Json.parse(userAnswersJson("no", "startedInClaim", "2020-03-15")).as[UserAnswers]
+    val claimPeriod = PayPeriod(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 4, 30))
+    val expected = FurloughPeriod(LocalDate.of(2020, 3, 15), claimPeriod.end)
+
+    extractFurloughPeriod(userAnswers) mustBe Some(expected)
   }
 }
