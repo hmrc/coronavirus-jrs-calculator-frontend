977b0030effdd8fdec0a90cbb624737f952e19a9
Pasquale Gatto <pasquale.gatto@hmrc.digital.gov.uk>
2020-04-20 18:08:29 +0100

    Add test

--- test/services/ReferencePayCalculatorSpec.scala
+++ test/services/ReferencePayCalculatorSpec.scala
@@ -61,4 +61,32 @@ class ReferencePayCalculatorSpec extends SpecBase with PeriodBuilder {
 
     payDateToDailyEarning(input) mustBe expected
   }
+
+  "calculate previous year wage with multiple pay dates" ignore new ReferencePayCalculator {
+//    Current year period 1 2020-3-1 to 2020-3-7  => paydate 2020, 3, 7
+//    Current year period 2 2020-3-8 to 2020-3-14 => paydate 2020, 3, 14
+//
+//    look back 2020-3-2 and 2020-3-9
+//    user will be asked earned on 2020-3-2 amount
+//      user will be asked earned on 2020-3-9 amount
+//      user will be asked earned on 2020-3-16 amount
+//      look back 2020-3-9 and 2020-3-16 => not to be shown in the UI
+//    ask amount => 2020-3-2  => 700.0 => (700.0 / 7) * 2 => 200.0
+//    ask amount => 2020-3-9  => 350.0 => (350.0 / 7) * 5 => 250.0
+//    Sliding
+//    ask amount => 2020-3-9  => 350.0 => (350.0 / 7) * 2 => 100.0
+//    ask amount => 2020-3-16 => 140.0 => (140.0 / 7) * 5 => 100.0
+
+    val dateOne = PaymentDate(LocalDate.of(2020, 3, 2))
+    val datetwo = PaymentDate(LocalDate.of(2020, 3, 9))
+    val datethree = PaymentDate(LocalDate.of(2020, 3, 16))
+    val payForDateOne = Amount(700.00)
+    val payForDateTwo = Amount(350.00)
+    val payForDateThree = Amount(140.00)
+    val input: Seq[(PaymentDate, Amount)] = List(dateOne -> payForDateOne, datetwo -> payForDateTwo, datethree -> payForDateThree)
+
+    val expected: Seq[(PaymentDate, BigDecimal)] = List(dateOne -> 200.0, datetwo -> 250.0, datethree -> 100.0)
+
+    payDateToDailyEarning(input) mustBe expected
+  }
 }
