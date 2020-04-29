00d09a33f29ed4b937603e6f2daa439554f87d73
Pasquale Gatto <pasquale.gatto@hmrc.digital.gov.uk>
2020-04-29 10:31:02 +0100

    Calculators: remove Amount for furlough in Partial periods

--- test/controllers/FurloughCalculationsControllerSpec.scala
+++ test/controllers/FurloughCalculationsControllerSpec.scala
@@ -10,7 +10,7 @@ import java.time.LocalDate
 import base.SpecBaseWithApplication
 import forms.FurloughCalculationsFormProvider
 import models.Calculation.FurloughCalculationResult
-import models.{Amount, CalculationResult, FullPeriod, FurloughCalculations, PaymentDate, Period, PeriodBreakdown, PeriodWithPaymentDate, UserAnswers}
+import models.{Amount, CalculationResult, FullPeriod, FullPeriodBreakdown, FullPeriodWithPaymentDate, FurloughCalculations, NormalMode, PaymentDate, Period}
 import navigation.{FakeNavigator, Navigator}
 import org.mockito.Matchers.any
 import org.mockito.Mockito.when
@@ -18,8 +18,8 @@ import org.scalatestplus.mockito.MockitoSugar
 import pages.FurloughCalculationsPage
 import play.api.inject.bind
 import play.api.mvc.{AnyContentAsEmpty, Call}
-import play.api.test.FakeRequest
 import play.api.test.CSRFTokenHelper._
+import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import repositories.SessionRepository
 import views.html.FurloughCalculationsView
@@ -30,7 +30,7 @@ class FurloughCalculationsControllerSpec extends SpecBaseWithApplication with Mo
 
   def onwardRoute = Call("GET", "/foo")
 
-  val furloughCalculationsRoute = routes.FurloughCalculationsController.onPageLoad().url
+  val furloughCalculationsRoute = routes.FurloughCalculationsController.onPageLoad(NormalMode).url
 
   val formProvider = new FurloughCalculationsFormProvider()
   val form = formProvider()
@@ -38,16 +38,18 @@ class FurloughCalculationsControllerSpec extends SpecBaseWithApplication with Mo
   val furlough =
     CalculationResult(FurloughCalculationResult, 3200.00, List(periodBreakdownOne(2000.00, 1600.00), periodBreakdownTwo(2000.00, 1600.00)))
   def periodBreakdownOne(grossPay: BigDecimal, grant: BigDecimal) =
-    PeriodBreakdown(
-      Amount(grossPay.setScale(2)),
+    FullPeriodBreakdown(
       Amount(grant.setScale(2)),
-      PeriodWithPaymentDate(FullPeriod(Period(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))), PaymentDate(LocalDate.of(2020, 3, 20)))
+      FullPeriodWithPaymentDate(
+        FullPeriod(Period(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 3, 31))),
+        PaymentDate(LocalDate.of(2020, 3, 20)))
     )
   def periodBreakdownTwo(grossPay: BigDecimal, grant: BigDecimal) =
-    PeriodBreakdown(
-      Amount(grossPay.setScale(2)),
+    FullPeriodBreakdown(
       Amount(grant.setScale(2)),
-      PeriodWithPaymentDate(FullPeriod(Period(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 4, 30))), PaymentDate(LocalDate.of(2020, 4, 20)))
+      FullPeriodWithPaymentDate(
+        FullPeriod(Period(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 4, 30))),
+        PaymentDate(LocalDate.of(2020, 4, 20)))
     )
 
   val getRequest: FakeRequest[AnyContentAsEmpty.type] =
@@ -67,7 +69,7 @@ class FurloughCalculationsControllerSpec extends SpecBaseWithApplication with Mo
       status(result) mustEqual OK
 
       contentAsString(result) mustEqual
-        view(form, furlough)(getRequest, messages).toString
+        view(form, NormalMode, furlough)(getRequest, messages).toString
 
       application.stop()
     }
@@ -85,7 +87,7 @@ class FurloughCalculationsControllerSpec extends SpecBaseWithApplication with Mo
       status(result) mustEqual OK
 
       contentAsString(result) mustEqual
-        view(form.fill(FurloughCalculations.values.head), furlough)(getRequest, messages).toString
+        view(form.fill(FurloughCalculations.values.head), NormalMode, furlough)(getRequest, messages).toString
 
       application.stop()
     }
@@ -135,7 +137,7 @@ class FurloughCalculationsControllerSpec extends SpecBaseWithApplication with Mo
       status(result) mustEqual BAD_REQUEST
 
       contentAsString(result) mustEqual
-        view(boundForm, furlough)(request, messages).toString
+        view(boundForm, NormalMode, furlough)(request, messages).toString
 
       application.stop()
     }
