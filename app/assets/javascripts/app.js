56b908001f7d73fb4fac0318fc42cab80d7b4540
sureshhmrc <27204388+sureshhmrc@users.noreply.github.com>
2020-04-30 11:27:34 +0100

    [CJRSC-7][SM]: Salary fields allowing more than 2 decimals

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -54,4 +54,19 @@ function numberInputs() {
         if ( e.which == 38 || e.which == 40 || e.which == 188 )
             e.preventDefault();
     });
+
+    $(document).on('keydown', 'input[pattern]', function(e){
+      var input = $(this);
+      var oldVal = input.val();
+      var regex = new RegExp(input.attr('pattern'), 'g');
+
+      setTimeout(function(){
+        var newVal = input.val();
+        if(!regex.test(newVal)){
+          input.val(oldVal);
+        }
+      }, 1);
+    });
+
+
 }
