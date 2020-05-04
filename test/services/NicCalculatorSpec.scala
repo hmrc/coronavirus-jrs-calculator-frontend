0d2d3d13741352556ab8cdb0c89bab27f015b0ab
Pasquale Gatto <pasquale.gatto@hmrc.digital.gov.uk>
2020-05-04 14:01:39 +0100

    CJRSC-123: Add test for additional payment or top up only for Nic

--- test/services/NicCalculatorSpec.scala
+++ test/services/NicCalculatorSpec.scala
@@ -34,6 +34,28 @@ class NicCalculatorSpec extends SpecBase with ScalaCheckPropertyChecks with Core
     calculatePartialPeriodNic(FourWeekly, Amount(1124.23), Amount(426.02), period, paymentDate) mustBe expected
   }
 
+  "calculates Nic with additional payment and 0.0 top up" in new NicCalculator {
+    val additionalPayment = Some(Amount(300))
+    val topUp = None
+    val nonFurlough = Amount(2200.0)
+    val furlough = Amount(720.0)
+    val pp = partialPeriod("2020,3,1" -> "2020,3,31", "2020,3,23" -> "2020, 3, 31")
+
+    calculatePartialPeriodNic(Monthly, nonFurlough, furlough, pp, PaymentDate(LocalDate.of(2020, 3, 31)), additionalPayment, topUp).grant mustBe Amount(
+      100.20)
+  }
+
+  "calculates Nic with top up and 0.0 additional payment" in new NicCalculator {
+    val additionalPayment = None
+    val topUp = Some(Amount(200.0))
+    val nonFurlough = Amount(2200.0)
+    val furlough = Amount(720.0)
+    val pp = partialPeriod("2020,3,1" -> "2020,3,31", "2020,3,23" -> "2020, 3, 31")
+
+    calculatePartialPeriodNic(Monthly, nonFurlough, furlough, pp, PaymentDate(LocalDate.of(2020, 3, 31)), additionalPayment, topUp).grant mustBe Amount(
+      75.28)
+  }
+
   "calculates Nic with additional payment plus top up" in new NicCalculator {
     val additionalPayment = Some(Amount(300))
     val topUp = Some(Amount(200))
