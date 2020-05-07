6d5f2b08aabb100663c7a209aeee108f1d77a32c
Pasquale Gatto <pasquale.gatto@hmrc.digital.gov.uk>
2020-05-07 11:38:24 +0100

    CJRSC-123_wiring: start passing top ups

--- app/handlers/ConfirmationControllerRequestHandler.scala
+++ app/handlers/ConfirmationControllerRequestHandler.scala
@@ -1,17 +1,6 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package handlers
@@ -19,7 +8,7 @@ package handlers
 import models.Calculation.{NicCalculationResult, PensionCalculationResult}
 import models.NicCategory.{Nonpayable, Payable}
 import models.PensionStatus.{DoesContribute, DoesNotContribute}
-import models.{Amount, CalculationResult, FullPeriodBreakdown, NicCategory, PartialPeriodBreakdown, PaymentFrequency, PensionStatus, Period, UserAnswers}
+import models.{AdditionalPayment, Amount, CalculationResult, FullPeriodBreakdown, NicCategory, PartialPeriodBreakdown, PaymentFrequency, PensionStatus, Period, TopUpPayment, UserAnswers}
 import services._
 import viewmodels.{ConfirmationDataResult, ConfirmationMetadata, ConfirmationViewBreakdown}
 
@@ -39,7 +28,7 @@ trait ConfirmationControllerRequestHandler
       payments = calculateReferencePay(data)
       furlough = calculateFurloughGrant(data.frequency, payments)
       niAnswer <- extractNicCategory(userAnswers)
-      ni = calculateNi(furlough, niAnswer, data.frequency)
+      ni = calculateNi(furlough, niAnswer, data.frequency, Seq.empty, Seq.empty)
       pensionAnswer <- extractPensionStatus(userAnswers)
       pension = calculatePension(furlough, pensionAnswer, data.frequency)
     } yield ConfirmationViewBreakdown(furlough, ni, pension)
@@ -54,9 +43,14 @@ trait ConfirmationControllerRequestHandler
       pensionStatus  <- extractPensionStatus(userAnswers)
     } yield ConfirmationMetadata(Period(claimStart, claimEnd), furloughPeriod, frequency, nicCategory, pensionStatus)
 
-  private def calculateNi(furloughResult: CalculationResult, nic: NicCategory, frequency: PaymentFrequency): CalculationResult =
+  private def calculateNi(
+    furloughResult: CalculationResult,
+    nic: NicCategory,
+    frequency: PaymentFrequency,
+    additionals: Seq[AdditionalPayment],
+    topUps: Seq[TopUpPayment]): CalculationResult =
     nic match {
-      case Payable => calculateNicGrant(frequency, furloughResult.payPeriodBreakdowns, Seq.empty, Seq.empty) //TODO wire with UI data
+      case Payable => calculateNicGrant(frequency, furloughResult.payPeriodBreakdowns, additionals, topUps)
       case Nonpayable =>
         CalculationResult(
           NicCalculationResult,
--- app/services/NicCalculator.scala
+++ app/services/NicCalculator.scala
@@ -1,17 +1,6 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package services
@@ -28,7 +17,7 @@ trait NicCalculator extends FurloughCapCalculator with CommonCalculationService 
     frequency: PaymentFrequency,
     furloughBreakdown: Seq[PeriodBreakdown],
     additionals: Seq[AdditionalPayment],
-    topUps: Seq[TopUpPayment]): CalculationResult = { //TODO remove dafaulted
+    topUps: Seq[TopUpPayment]): CalculationResult = {
     val nicBreakdowns = furloughBreakdown.map {
       case FullPeriodBreakdown(grant, periodWithPaymentDate) =>
         calculateFullPeriodNic(
