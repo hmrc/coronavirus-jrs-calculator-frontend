4e3ae32b03d9472ddab5692334cdcf57bcc1f312
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-06-02 09:07:52 +0100

    CJRSC-226: remove more stuff

--- test/controllers/AdditionalPaymentAmountControllerSpec.scala
+++ test/controllers/AdditionalPaymentAmountControllerSpec.scala
@@ -18,8 +18,7 @@ package controllers
 
 import java.time.LocalDate
 
-import base.{CoreTestDataBuilder, SpecBaseControllerSpecs}
-import com.typesafe.config.ConfigValue
+import base.SpecBaseControllerSpecs
 import config.FrontendAppConfig
 import controllers.actions._
 import forms.AdditionalPaymentAmountFormProvider
@@ -29,7 +28,7 @@ import org.mockito.Matchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.Configuration
-import play.api.mvc.{AnyContentAsEmpty, Call}
+import play.api.mvc.AnyContentAsEmpty
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -38,7 +37,7 @@ import repositories.SessionRepository
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class AdditionalPaymentAmountControllerSpec extends SpecBaseControllerSpecs with MockitoSugar with CoreTestDataBuilder {
+class AdditionalPaymentAmountControllerSpec extends SpecBaseControllerSpecs with MockitoSugar {
 
   val formProvider = new AdditionalPaymentAmountFormProvider()
   val form = formProvider()
