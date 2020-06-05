91686e64171a2cff4003c364cff028aeb0c97d87
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2020-06-05 14:52:46 +0100

    [CJRSC-241][JR] Phase Two Pension Calculation

--- test/services/PensionCalculatorSpec.scala
+++ test/services/PensionCalculatorSpec.scala
@@ -17,12 +17,14 @@
 package services
 
 import base.{CoreTestDataBuilder, SpecBase}
-import models.PaymentFrequency.Monthly
+import models.PaymentFrequency.{Monthly, Weekly}
 import models.PensionStatus.{DoesContribute, DoesNotContribute}
-import models.{Amount, FullPeriodPensionBreakdown, PartialPeriodPensionBreakdown, TaxYearEnding2020, TaxYearEnding2021}
+import models.{Amount, FullPeriodCap, FullPeriodPensionBreakdown, Hours, PartialPeriodPensionBreakdown, PhaseTwoFurloughBreakdown, PhaseTwoPeriod, RegularPaymentWithPhaseTwoPeriod, TaxYearEnding2020, TaxYearEnding2021}
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class PensionCalculatorSpec extends SpecBase with ScalaCheckPropertyChecks with CoreTestDataBuilder {
+class
+
+PensionCalculatorSpec extends SpecBase with ScalaCheckPropertyChecks with CoreTestDataBuilder {
 
   "Returns 0.00 for Pension grant if not eligible for Pension grant full period" in new PensionCalculator {
     val payment = regularPaymentWithFullPeriod(2000.00, 2000.00, fullPeriodWithPaymentDate("2020-03-01", "2020-03-31", "2020-03-31"))
@@ -90,5 +92,24 @@ class PensionCalculatorSpec extends SpecBase with ScalaCheckPropertyChecks with 
       Amount(16.20)
     )
   )
+
+  "Phase Two: calculate pension grant based on part time furlough grant" in new PensionCalculator {
+    val furloughBreakdowns = Seq(
+      PhaseTwoFurloughBreakdown(
+        Amount(200.0),
+        RegularPaymentWithPhaseTwoPeriod(
+          Amount(500.00),
+          Amount(250.0),
+          PhaseTwoPeriod(
+            fullPeriodWithPaymentDate("2020,7,1", "2020,7,7", "2020,7,1"),
+            Some(Hours(20.0)),
+            Some(Hours(40.0))
+          )
+        ),
+        FullPeriodCap(288.46)
+      )
+    )
+    phaseTwoPension(furloughBreakdowns, Weekly, DoesContribute).total mustBe 4.20
+  }
 
 }
