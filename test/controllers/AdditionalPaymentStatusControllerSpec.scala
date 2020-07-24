be385b6d5529cc1848e9d3a28f8427ce5234f052
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-07-24 08:56:14 +0100

    CJRSC-318: stub in AnnualPayAmountControllerSpec

--- test/controllers/AdditionalPaymentStatusControllerSpec.scala
+++ test/controllers/AdditionalPaymentStatusControllerSpec.scala
@@ -26,6 +26,7 @@ import play.api.mvc.AnyContentAsEmpty
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import services.UserAnswerPersistence
 import views.html.AdditionalPaymentStatusView
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -61,7 +62,9 @@ class AdditionalPaymentStatusControllerSpec extends SpecBaseControllerSpecs {
       formProvider,
       component,
       view
-    )
+    ) {
+      override val userAnswerPersistence = new UserAnswerPersistence(_ => Future.successful(true))
+    }
 
   "AdditionalPaymentStatusController" must {
 
