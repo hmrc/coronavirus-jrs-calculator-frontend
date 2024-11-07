7302a7755ba2cbec9c863c341efdac477a531c4b
@diksha-diksha <176032121+diksha-diksha@users.noreply.github.com>
2024-11-07 16:16:35 +0000

    DL-14424: updated dependency to avoid bobby rule violation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,13 +11,13 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% s"play-conditional-form-mapping$playSuffix"    % "2.0.0",
     "uk.gov.hmrc"                   %% s"bootstrap-frontend$playSuffix"               % bootstrapVersion,
     "uk.gov.hmrc"                   %% s"play-frontend-hmrc$playSuffix"               % "8.4.0",
-    "org.typelevel"                 %% "cats-core"                                    % "2.10.0",
-    "com.softwaremill.quicklens"    %% "quicklens"                                    % "1.9.6",
-    "com.github.pureconfig"         %% "pureconfig"                                   % "0.17.4",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                         % "2.16.0",
-    "org.playframework"             %% "play-json"                                    % "3.0.2",
-    "ch.qos.logback"                % "logback-classic"                               % "1.4.14",
-    "com.typesafe.play"             %% "play-logback"                                 % "2.9.3",
+    "org.typelevel"                 %% "cats-core"                                    % "2.12.0",
+    "com.softwaremill.quicklens"    %% "quicklens"                                    % "1.9.7",
+    "com.github.pureconfig"         %% "pureconfig"                                   % "0.17.7",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                         % "2.17.2",
+    "org.playframework"             %% "play-json"                                    % "3.0.4",
+    "ch.qos.logback"                % "logback-classic"                               % "1.5.6",
+    "com.typesafe.play"             %% "play-logback"                                 % "2.9.5",
   )
 
   val test: Seq[ModuleID] = Seq(
